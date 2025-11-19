42a68193ed6171d97e8482dac90321ca09b86138
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-19 10:21:15 -0300

    Update loader.py

--- PETdor_2.0/especies/loader.py
+++ PETdor_2.0/especies/loader.py
@@ -1,5 +1,17 @@
-# PetDor/especies/loader.py
-from .index import Especie
+# PETdor_2.0/especies/loader.py
+
+"""
+Módulo para carregar e gerenciar dados de espécies.
+"""
+
+from .index import get_especies_nomes # Importa de especies.index agora!
 
 def listar_especies():
-    return [e.value for e in Especie]
+    """
+    Retorna uma lista de nomes de espécies disponíveis para seleção.
+    """
+    return get_especies_nomes()
+
+# Exemplo de uso (opcional, para testes)
+if __name__ == "__main__":
+    print("Espécies listadas pelo loader:", listar_especies())

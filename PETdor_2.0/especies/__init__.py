0b262ec6fecb173c134b2013d1d155ecc8d3b55f
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-19 10:20:57 -0300

    Update __init__.py

--- PETdor_2.0/especies/__init__.py
+++ PETdor_2.0/especies/__init__.py
@@ -1,18 +1,26 @@
-from .index import get_especies_nomes, get_especie_config
+# PETdor_2.0/especies/__init__.py
 
-# Importa configurações individuais das espécies
-from .gato import CONFIG_GATOS
-from .coelho import CONFIG_COELHO
-from .porquinho_india import CONFIG_PORQUINHO
-from .aves import CONFIG_AVES
-from .repteis import CONFIG_REPTEIS
+"""
+Módulo de inicialização do pacote 'especies'.
+Expõe funções para listar e obter configurações de espécies.
+"""
 
+# Importa o módulo index, que agora contém as classes Pergunta e EspecieConfig
+# e as funções de registro e busca de espécies.
+from . import index
+from . import loader # Importa o módulo loader para que ele seja parte do pacote
+
+# Exponha as funções e classes que você deseja que sejam acessíveis diretamente
+# do pacote 'especies'.
 __all__ = [
+    "index",
+    "loader",
+    "Pergunta", # Expondo Pergunta e EspecieConfig diretamente do pacote
+    "EspecieConfig",
     "get_especies_nomes",
     "get_especie_config",
-    "CONFIG_GATOS",
-    "CONFIG_COELHO",
-    "CONFIG_PORQUINHO",
-    "CONFIG_AVES",
-    "CONFIG_REPTEIS",
 ]
+
+# Importa as funções e classes específicas para o namespace do pacote
+# para que possam ser acessadas como especies.get_especies_nomes()
+from .index import Pergunta, EspecieConfig, get_especies_nomes, get_especie_config

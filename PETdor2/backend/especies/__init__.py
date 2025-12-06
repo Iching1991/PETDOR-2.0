bc961b0efd7dc7cbe35b3185275d399d477d7d78
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-12-05 23:43:53 -0300

    Refactor __init__.py with documentation and imports
    Added package documentation and reorganized imports.

--- PETdor2/backend/especies/__init__.py
+++ PETdor2/backend/especies/__init__.py
@@ -1,13 +1,18 @@
 # PETdor2/especies/__init__.py
-from .base import EspecieConfig, Pergunta
+"""
+Pacote de espécies do PETDor2.
+Registra e disponibiliza todas as espécies do sistema.
+"""
+
 from .index import (
+    EspecieConfig,
+    Pergunta,
     registrar_especie,
     buscar_especie_por_id,
     listar_especies,
     get_especies_nomes,
     get_especies_ids,
     get_escala_labels,
-    carregar_especies
 )
 
 __all__ = [
@@ -19,5 +24,4 @@ __all__ = [
     "get_especies_nomes",
     "get_especies_ids",
     "get_escala_labels",
-    "carregar_especies",
 ]

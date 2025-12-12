8d4683606ed83af54a5ad5c15c9c7fd972f67ca3
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-12-12 00:12:57 -0300

    Update __init__.py

--- PETdor2/backend/utils/__init__.py
+++ PETdor2/backend/utils/__init__.py
@@ -1,1 +1,32 @@
-from .email_sender import enviar_email
+# backend/utils/__init__.py
+
+from .email_sender import (
+    enviar_email_confirmacao_generico,
+    enviar_email_recuperacao_senha,
+)
+
+from .config import (
+    SMTP_SERVIDOR,
+    SMTP_PORTA,
+    SMTP_EMAIL,
+    SMTP_SENHA,
+    SMTP_USAR_SSL,
+    APP_CONFIG,
+    STREAMLIT_APP_URL,
+    SUPABASE_URL,
+    SUPABASE_KEY,
+)
+
+__all__ = [
+    "enviar_email_confirmacao_generico",
+    "enviar_email_recuperacao_senha",
+    "SMTP_SERVIDOR",
+    "SMTP_PORTA",
+    "SMTP_EMAIL",
+    "SMTP_SENHA",
+    "SMTP_USAR_SSL",
+    "APP_CONFIG",
+    "STREAMLIT_APP_URL",
+    "SUPABASE_URL",
+    "SUPABASE_KEY",
+]

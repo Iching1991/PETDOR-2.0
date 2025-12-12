6a75505e164c445d6f8143fd5cbda26af0591a72
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-12-12 00:02:46 -0300

    Update __init__.py

--- PETdor2/backend/database/__init__.py
+++ PETdor2/backend/database/__init__.py
@@ -1,8 +1,19 @@
+# backend/database/__init__.py
+
 from .supabase_client import (
     get_supabase,
     testar_conexao,
     supabase_table_select,
     supabase_table_insert,
     supabase_table_update,
-    supabase_table_delete
+    supabase_table_delete,
 )
+
+__all__ = [
+    "get_supabase",
+    "testar_conexao",
+    "supabase_table_select",
+    "supabase_table_insert",
+    "supabase_table_update",
+    "supabase_table_delete",
+]

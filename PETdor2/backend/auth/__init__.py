7fdc16601763071c6f3a506d5c19f02ec8d315d8
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-12-12 00:03:07 -0300

    Update __init__.py

--- PETdor2/backend/auth/__init__.py
+++ PETdor2/backend/auth/__init__.py
@@ -1,7 +1,17 @@
+# backend/auth/__init__.py
+
 from .user import (
     criar_usuario,
-    autenticar_usuario,
     buscar_usuario_por_email,
+    autenticar_usuario,
     atualizar_usuario,
-    deletar_usuario
+    deletar_usuario,
 )
+
+__all__ = [
+    "criar_usuario",
+    "buscar_usuario_por_email",
+    "autenticar_usuario",
+    "atualizar_usuario",
+    "deletar_usuario",
+]
